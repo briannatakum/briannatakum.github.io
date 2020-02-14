c4791ac459bf6ac38d2ea62e62f31b6d1fef3ea2
Brianna Takum <briannatakum@gmail.com>
2020-02-14 17:07:41 +0700

    init

---
+++ precache-manifest.592c0e7f072b40daf3edc23ab5734ce1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4e86cc6a095db333f3b34ef164a704",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71e0fe7addfab47b85e1",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1ee36312d20b67175d77",
+    "url": "/static/js/2.8aa8e2b2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8aa8e2b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e0fe7addfab47b85e1",
+    "url": "/static/js/main.9f002ed0.chunk.js"
+  },
+  {
+    "revision": "7a625a7962af18a9bd48",
+    "url": "/static/js/runtime-main.201845b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
